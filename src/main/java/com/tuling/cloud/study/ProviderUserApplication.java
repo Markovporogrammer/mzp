788d0900940d5516506f0b412a2484004236bf1f
Markovporogrammer <13783880460@163.com>
2018-08-07 22:16:50 +0800

    user 3 synchronzing update

--- src/main/java/com/tuling/cloud/study/ProviderUserApplication.java
+++ src/main/java/com/tuling/cloud/study/ProviderUserApplication.java
@@ -7,5 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ProviderUserApplication {
- 
+ public static void main(String[] args) {
+	
+}
 }
