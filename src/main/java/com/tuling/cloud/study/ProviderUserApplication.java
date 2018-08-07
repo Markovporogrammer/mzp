91c4ab0782ebcf7e867b6d9c7d330b20ed67561a
Markovporogrammer <13783880460@163.com>
2018-08-07 21:50:02 +0800

    mzp 2 update commit

--- src/main/java/com/tuling/cloud/study/ProviderUserApplication.java
+++ src/main/java/com/tuling/cloud/study/ProviderUserApplication.java
@@ -7,7 +7,5 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ProviderUserApplication {
-  public static void main(String[] args) {
-    SpringApplication.run(ProviderUserApplication.class, args);
-  }
+ 
 }
